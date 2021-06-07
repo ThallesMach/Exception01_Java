ff1f301a0f76dc45a1065c020723175c9e361941
machado <machado@HP-Pavilion-dm4-Notebook-PC>
2021-06-07 17:38:49 -0300

    add initial Exceptions

---
+++ Exceptions01_Java/src/application/Program.java
@@ -0,0 +1,34 @@
+
+//	--->Seção-15 Aula 142 - Criando exceções personalizadas 	--->07/06/2021.
+
+package application;
+
+/**
+ * @author Thalles machado
+ *
+ */
+public class Program {
+
+	public static void main(String[] args) {
+
+		System.out.println("-----------------------------------------------------------------------------");
+		System.out.println("--->Seção-15 Aula 142 - Exceptions     ");
+		System.out.println("-----------------------------------------------------------------------------");
+		System.out.println("*** No PDF tem introdução  *** ");
+		System.out.println("---------------------------------------------------------------");
+		System.out.println();
+		
+		
+		
+		
+		
+		System.out.println("        ---> FIM <--- ");
+		System.out.println("------------------------------------------------------------------------");
+	}
+
+}
+
+/**
+ * @author Thalles machado
+ *
+ */
